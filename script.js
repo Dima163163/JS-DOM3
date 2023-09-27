a2106c6a91ab21686a0132261480582d23839f40
Dima163163 <khlopovdima@gmail.com>
2023-09-27 15:19:25 +0400

    Dom3 list

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+const list = document.querySelector('.list');
+let newStr;
+
+//  Функция добавления элемента в список
+const setStrToList = (str) => {
+  const newStr = str.trim();
+  if (newStr !== null && newStr !== 'del' && newStr !== '') {
+    list.insertAdjacentHTML('beforeend', `<li class="list__item">
+    ${newStr}</li>`);
+  }
+};
+
+// Функция удаления последнего элемента из списка
+const delStr = () => {
+  const listItems = list.getElementsByTagName('li');
+  const lastItem = listItems[listItems.length - 1];
+  list.removeChild(lastItem);
+};
+
+// Функция отчистки списка
+const clearList = () => {
+  list.textContent = '';
+};
+
+// Бесконечный цикл
+while (true) {
+  newStr = prompt('Введите строку для добавления в список: ');
+  if (newStr === 'del') {
+    delStr();
+    break;
+  }
+  if (newStr === 'clear') {
+    clearList();
+    break;
+  }
+  if (newStr === null || newStr === 'exit') break;
+  setStrToList(newStr);
+}
